7ca4610dc7e9615303f6915d975d19b412b0fa83
onenoteperpage <danijel.james+onenoteperpage@gmail.com>
2024-03-20 15:14:43 +1100

    init commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace ocdb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "User Id=dbadmin;Password=db_password;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=rds_hostname.rds.amazonaws.com)(PORT=1521)))(CONNECT_Data=(SID=ORCL)));";
+
+            string sql = @"SELECT username FROM dba_users";
+
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+
+                using (OracleCommand command = new OracleCommand(sql, connection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("List of usernames:");
+                        Console.WriteLine("-------------------");
+
+                        while (reader.Read())
+                        {
+                            string username = reader.GetString(0);
+                            Console.WriteLine(username);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ocdb
+
+Oracle Database Connection Test needs to have the connection string updated, in the Program.cs file, then use it to test a connection to an OracleDB hosted in AWS, and it will retrieve a list of all the usernames in the DB.
